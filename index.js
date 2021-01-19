5b6b6dcc8d905ca243e9e87b9fe2d4ac471ec264
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-19 22:38:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma() {
+  var uppercase = "HELLO!"
+  var lowercase = "hello!"
+  var mixedCase = "Hi"
+  
+  if(string) {
+    
+  }
+  else if{
+    
+  }
+  else {
+  }
+}

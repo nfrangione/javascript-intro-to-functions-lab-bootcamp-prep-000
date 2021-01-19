06723fb4cbd1f2211812dc256978002ee07b7c94
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-19 22:53:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,10 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase() === lowercase) {
     return "I can't hear you!"
   }
-  
-  else if (string.toUpperCase() === uppercase) {
+  if (string.toUpperCase() === uppercase) {
     return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma.") {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }

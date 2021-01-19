16d32150439afb25c0ff85de690c0697e146479b
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-19 22:43:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ function sayHiToGrandma() {
   else if (string.toLowerCase === lowercase) {
     return "I can't hear you!"
   }
-  else if {string === "I love you, Grandma"
+  else if (string === "I love you, Grandma.") {
+    return "I love you too!"
+  }
+  else {
     
   }
 }
